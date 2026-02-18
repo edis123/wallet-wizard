946ce2a204fecf21a6c7a4e89b98f696e7e04af0
edis123 <edismur2@gmail.com>
2026-02-18 15:22:45 -0500

    Changes in server.js

--- server/server.js
+++ server/server.js
@@ -5,7 +5,8 @@ const cors = require('cors');//connects backend to frontend
 const PORT = 3100
 
 app.use(cors())
-
+const path = require('path');
+const result = require('dotenv').config({ path: path.resolve(__dirname, '../server/.env.development') });
 
 app.get("/api/home", (req, res)=>{
 
@@ -13,7 +14,20 @@ app.get("/api/home", (req, res)=>{
 
 })
 
-
+app.get("/api/transaction", async (req, res) => {
+  let client
+  try {
+    client = await pool.connect();
+    console.log('Got a connection from the pool');
+    const resp = await client.query('SELECT * FROM transaction');
+    res.json(resp.rows);
+  } catch (err) {
+    console.error(err);
+    res.json({error: err});
+  } finally {
+    client?.release();
+  }
+});
 app.listen(PORT,()=>{
 
  console.log(`Server is running on port ${PORT}`)
