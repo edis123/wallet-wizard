bdbf19d688c73c128ee6ba4a96f3cc5cb6b475e5
edis123 <edismur2@gmail.com>
2026-02-18 23:21:15 -0500

    created db.js with new prisma 7 config, updated server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();//backend
-
+const prisma  = require("./db")
 const cors = require('cors');//connects backend to frontend
 const PORT = 3100
 
@@ -14,20 +14,47 @@ app.get("/api/home", (req, res)=>{
 
 })
 
-app.get("/api/transaction", async (req, res) => {
-  let client
-  try {
-    client = await pool.connect();
-    console.log('Got a connection from the pool');
-    const resp = await client.query('SELECT * FROM transaction');
-    res.json(resp.rows);
-  } catch (err) {
-    console.error(err);
-    res.json({error: err});
-  } finally {
-    client?.release();
-  }
-});
+app.get("/api/transaction", async(req, res )=>{
+   try{
+
+    // console.log("HIT /api/transaction on", req.socket.localPort);
+ const trans = await prisma.transaction.findMany();
+ 
+    if(trans.length ===0){
+       console.log("Empty table")
+       return res.status(200).json({message: "Empty Table"})
+       
+    }
+    res.json(trans)
+ 
+   }
+   catch(error){
+
+    console.error("failed to get transactions ",error)
+    res.status(500).json({error:"Failed to get Transaction"})
+   }
+})
+
+
+
+
+
+
+
+// app.get("/api/transaction", async (req, res) => {
+//   let client
+//   try {
+//     client = await prisma.connect();
+//     console.log('Got a connection from the DB');
+//     const resp = await client.query('SELECT * FROM transaction');
+//     res.json(resp.rows);
+//   } catch (err) {
+//     console.error(err);
+//     res.json({error: err});
+//   } finally {
+//     client?.release();
+//   }
+// });
 app.listen(PORT,()=>{
 
  console.log(`Server is running on port ${PORT}`)
