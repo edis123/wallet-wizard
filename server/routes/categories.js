2139d53683e143bde6c947ab404659473a0f5c41
edis123 <edismur2@gmail.com>
2026-02-25 19:17:10 -0500

    CRUD in Category completed

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -5,7 +5,7 @@ const prisma = require("../db");
 
 const User_ID = process.env.User_ID;
 
-
+// Create
 
 router.post("/", async (req,res)=>{
 try{
@@ -15,7 +15,7 @@ try{
  
   if(name==="" || name===null || !type ) return res.status(400).json({error:"Enter valid Name and Type!"})
 
-   
+
     const cat = await prisma.category.create({
 
    data:{
@@ -24,7 +24,6 @@ try{
     name,
     type
    }
-
     })
  
      res.status(201).json(cat);
@@ -35,4 +34,106 @@ catch(error){
     res.status(500).json({error:error.message})
 }
 
+}) 
+
+
+// Get by name
+
+router.get("/:name", async (req,res)=>{
+
+    try{
+    
+        if(!User_ID)  return res.status(401).json({error:"Unauthorized Access!"})
+     
+        const catName =req.params.name
+
+        let cat = await  prisma.category.findFirst({
+
+           where:{ 
+            userId:User_ID,
+            name:catName
+           }
+        })
+ 
+        res.status(200).json(cat)
+
+    }catch(error){
+
+        console.error(error)
+        res.status(500).json({error:error.message})
+    }
+
 })
+
+//update
+router.put("/:name", async (req,res)=>{
+
+    try{
+    
+        if(!User_ID)  return res.status(401).json({error:"Unauthorized Access!"})
+        const oldName = req.params.name
+        const {name,type} = req.body
+
+       if(name==="" || name===null || !type ) return res.status(400).json({error:"Enter valid Name and Type!"})
+
+        let cat = await  prisma.category.findUnique({
+           where:{ userId_name:{  //unique index for faster search
+            userId:User_ID,
+            name:oldName
+           }
+            
+           }
+        })
+       
+        if(!cat) return res.status(404).json({error:"Category not found!"})
+
+
+        const updated = await prisma.category.update({
+      
+            where:{userId_name:{userId:User_ID,name:oldName}},
+            data:{
+            
+            name,
+            type
+
+            }
+        })
+        res.status(200).json(updated)
+
+    }catch(error){
+
+        console.error(error)
+        res.status(500).json({error:error.message})
+    }
+
+})
+
+// Delete 
+
+
+router.delete("/:name", async (req,res)=>{
+
+    try{
+    
+        if(!User_ID)  return res.status(401).json({error:"Unauthorized Access!"})
+     
+        const catName = req.params.name
+
+        let catDeleted = await  prisma.category.delete({
+
+           where:{ userId_name:{ userId:User_ID, name:catName} }
+
+        })
+ 
+        res.status(200).json(catDeleted)
+
+    }catch(error){
+
+        console.error(error)
+        res.status(500).json({error:error.message})
+    }
+
+})
+
+
+module.exports = router;
