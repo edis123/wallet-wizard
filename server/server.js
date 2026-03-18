43ddc12c62f2680bf00557846320002ca1c17776
edis123 <edismur2@gmail.com>
2026-03-18 02:13:07 -0400

    retried server.js 3

--- server/server.js
+++ server/server.js
@@ -18,6 +18,16 @@ const corsOptions = {
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://wallet-wizard-zxes.onrender.com");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+  next();
+});
+
 app.use(cors(corsOptions));
 app.use(express.json());
 
