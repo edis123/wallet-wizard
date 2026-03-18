3b37c3210cda06a8b8ffd75fdebf09115e6f713c
edis123 <edismur2@gmail.com>
2026-03-18 14:00:30 -0400

    register form pop-up 5

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { useEffect,useState } from 'react'
-
+import { useRouter } from "next/router";
 function Index() {
 
-
+  const router = useRouter()
   const [message, setMessage] = useState("Loading)(HOLD)...")
 
   useEffect(()=>{
@@ -15,11 +15,15 @@ function Index() {
     .then((response)=>response.json())
     .then((data)=>{
       setMessage(data.message+" This is a test")
+  
     })
+  router.push("/login")
   },[])
   return (
     <div>{message}</div>
   )
+
+  
 
 //   useEffect(() => {
 //     apiMethods.api("/api/transactions")
