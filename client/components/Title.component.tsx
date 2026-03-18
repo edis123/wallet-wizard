1c4d7a86172f6b1ff076124ee22dda5a2dfad7df
edis123 <edismur2@gmail.com>
2026-03-18 03:44:37 -0400

    title centerd

--- client/components/Title.component.tsx
+++ client/components/Title.component.tsx
@@ -25,7 +25,7 @@ function Title() {
 
   if (error) return <div>{error}</div>;
 
-  return <div className="flex item-center">{title.title}</div>;
+  return <div className="flex item-center justify-center">{title.title}</div>;
 }
 
 export default Title;
